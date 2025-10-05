58901e05b07c6798354da3c0087c4769b8388649
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-10-05 18:34:07 +0200

    updates

--- lua/plugins/assistance.lua
+++ lua/plugins/assistance.lua
@@ -18,7 +18,7 @@ return {
             {
                 "<leader>ap",
                 function()
-                    require("sidekick.cli").select_prompt()
+                    require("sidekick.cli").prompt()
                 end,
                 desc = "Sidekick Ask Prompt",
                 mode = { "n", "v" },
