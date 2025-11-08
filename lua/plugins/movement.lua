644509506825cbbda5f486bb867568767add9ab0
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-11-08 12:16:11 +0100

    updates

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -3,7 +3,8 @@ return {
         "ggandor/leap.nvim",
         event = "InsertEnter",
         config = function()
-            require("leap").create_default_mappings()
+            vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+            vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
         end,
     },
 }
